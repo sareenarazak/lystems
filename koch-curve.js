5014d66c650d2c2a6c014bd307581c6860cf17f8
sareenarazak <11263177+sareenarazak@users.noreply.github.com>
2024-03-12 17:54:52 -0400

    Refactor Lsystem class
    With help from Reed

--- koch-curve.js
+++ koch-curve.js
@@ -10,3 +10,55 @@ const generations  = 10;
 const testTurtle = new Turtle(ctx, 10, 10, 0 );
 
 lystem.draw(generations, testTurtle);
+import { Turtle } from "./turtle.js";
+import * as Lystem from "./lystem.js"
+const canvas = document.getElementById("lystem-canvas");
+
+const generations  = 6;
+const length = 5;
+let angle = 90;
+function animateAngle() {
+    const testTurtle = new Turtle(canvas, canvas.width/2, 10, 0 );
+    console.log(angle);
+    let lystem = Lystem.createContext({
+        axiom: "F",
+        turtle: testTurtle,
+        angle: angle++,
+        length: length,
+        rules: {
+            "F": "F+F-F-F+F"
+        },
+        symbolTable: {
+            ...Lystem.BaseSymbolTable,
+            '+': ({ angle, turtle }) => turtle.turnLeft(angle),
+            '-': ({ angle, turtle }) => turtle.turnRight(angle),
+            "F" : ({length, turtle}) => turtle.forward(length)
+        }
+    })
+    testTurtle.setupCanvas();
+    const generated = Lystem.generate(lystem, generations);
+    Lystem.draw(lystem, generated);
+
+    setTimeout(() =>
+        requestAnimationFrame(animateAngle), 17)
+}
+animateAngle();
+
+let lystem = Lystem.createContext({
+    axiom: "F",
+    turtle: testTurtle,
+    angle: angle++,
+    length: length,
+    rules: {
+        "F": "F+F-F-F+F"
+    },
+    symbolTable: {
+        ...Lystem.BaseSymbolTable,
+        '+': ({ angle, turtle }) => turtle.turnLeft(angle),
+        '-': ({ angle, turtle }) => turtle.turnRight(angle),
+        "F" : ({length, turtle}) => turtle.forward(length)
+    }
+})
+testTurtle.setupCanvas();
+const generated = Lystem.generate(lystem, generations);
+Lystem.draw(lystem, generated);
